49a064c0b8f4a2928cec7e8723b275069b170329
Bahman World <bahman.world@gmail.com>
2024-02-28 13:00:58 +0000

    first commit from codespace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className='text-3xl'>Home Page</div>
-        <Link href={"/about"}>Go About Page</Link>
+        <Link href={"/about"}>Go About Page from Gihub Codespaces</Link>
     </main>
   );
 }

60a36e55b2e3f2c2bacc724b9019a525ae6ff99e
Bahman World <bahman.world@gmail.com>
2024-05-04 21:46:51 +0330

    routes updated

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server"
 
-export const GET = ()=>{
-    return NextResponse.json({ message: 'Hello World!' })
+export const GET = async()=>{
+    const json = await fetch("https://ifconfig.co/json")
+    const data = await json.json()
+    return NextResponse.json({ data })
 }

f5734d60f28865db7a1b739ace1283814598299b
Bahman World <bahman.world@gmail.com>
2024-05-10 12:49:14 +0330

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,17 @@
 "use client";
 
 import React from "react";
-import { Movie } from "@prisma/client";
-
 export default function Home() {
-  const [movie, setMovie] = React.useState<Movie | null>(null);
-  const [loading, setLoading] = React.useState<boolean>(false);
-
-  console.log("New Movie:", movie)
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="text-3xl">Home Page</div>
       <button
         onClick={() => {
-          setLoading(true);
-          fetch("/api/movie/create")
-            .then((res) => res.json())
-            .then(setMovie)
-            .finally(() => setLoading(false));
+          alert("Hello World");
         }}
       >
         Create New Movie
       </button>
-      <div>{loading ? "loading..." : movie?.name}</div>
     </main>
   );
 }
